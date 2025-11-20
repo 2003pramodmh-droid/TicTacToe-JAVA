0e29612121c701f62c876460c40303e465788b80
2003pramodmh-droid <2003.pramodmh@gmail.com>
2025-11-20 22:00:18 +0530

    Add files via upload

---
+++ src/com/tap/LaunchGame.java
@@ -0,0 +1,121 @@
+package com.tap;
+
+import java.util.Scanner;
+
+class TicTacToe{
+	static char[][] board;
+	public TicTacToe() {
+		board=new char[3][3];
+		initBoard();
+	}
+	void initBoard(){
+		for(int i=0;i<board.length;i++) {
+			for(int j=0;j<board[i].length;j++) {
+				board[i][j]=' ';
+			}
+		}
+	}
+	static void dispBoard() {
+		System.out.println("-------------");
+		for(int i=0;i<board.length;i++) {
+			System.out.print("| ");
+			for(int j=0;j<board[i].length;j++) {
+				System.out.print(board[i][j]+" | ");
+			}System.out.println();
+			System.out.println("-------------");
+		}
+	}
+	static void placeMark(int row,int col,char mark) {
+		if(row >=0&& row<=2&&col>=0&&col<=2) {
+			board[row][col]=mark;
+		}
+		else {
+			System.out.println("Invalid input");
+		}
+	}
+	static boolean checkRowWin() {
+		for(int i=0;i<board.length;i++) {
+			if(board[i][0]!=' '&& board[i][0]==board[i][1]&&board[i][1]==board[i][2]) {
+				return true;
+			}
+		}
+		return false;
+	}
+	static boolean checkColWin() {
+		for(int j=0;j<board.length;j++) {
+			if(board[0][j]!=' '&& board[0][j]==board[1][j]&&board[1][j]==board[2][j]) {
+				return true;
+			}
+		}
+		return false;
+	}
+	static boolean checkDiagWin() {
+		if(board[0][0]!=' '&&board[0][0]==board[1][1]
+				&&board[1][1]==board[2][2]||board[0][2]!=' '
+				&&board[0][2]==board[1][1]&&board[1][1]==board[2][0]) {
+			return true;
+		}
+		return false;
+	}
+}
+class HumanPlayer{
+	String name;
+	char mark;
+	
+	HumanPlayer(String name,char mark){
+		this.name=name;
+		this.mark=mark;
+	}
+	void makeAMove(){
+		Scanner scan=new Scanner(System.in);
+		int row;
+		int col;
+		do {
+			System.out.println("Enter the values");
+			row=scan.nextInt();
+			col=scan.nextInt();
+		} while (!isValidMove(row, col));
+		TicTacToe.placeMark(row, col, mark);
+	}
+	boolean isValidMove(int row , int col) {
+		if(row>=0 && row<=2 &&
+				col >=0 && col<=2) {
+			if(TicTacToe.board[row][col]==' ') {
+				return true;
+			}
+		}
+		return false;
+	}
+}
+public class LaunchGame {
+	public static void main(String[] args) {
+		TicTacToe t=new TicTacToe();
+		HumanPlayer p1=new HumanPlayer("pramod",'X');
+		HumanPlayer p2=new HumanPlayer("pummy",'O');
+		
+		HumanPlayer cp;
+		cp=p1;
+		
+		while(true){
+			System.out.println(cp.name + " turn");
+			cp.makeAMove();
+			TicTacToe.dispBoard();
+			
+			if(TicTacToe.checkColWin()||TicTacToe.checkRowWin()||TicTacToe.checkDiagWin()) {
+				System.out.println(cp.name + " has won");
+				break;
+			}
+			else {
+				if(cp==p1) {
+					cp=p2;
+				}
+				else
+				{
+					cp=p1;
+				}
+			}
+		}
+		
+	}
+
+}
